07767d28c85c1e6ca17bf26f9c8c1ef0665747ec
Choi Min Kyu <chlalsrb118@gmail.com>
2025-02-07 20:17:19 +0900

    🎉 Init: Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="grid  min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
+      메인 페이지
+      <div>tmp1</div>
+      <div>tmp2</div>
+    </div>
+  );
+}
---
+++ src/styles/font.ts
@@ -0,0 +1,28 @@
+import localFont from "next/font/local";
+
+export const SpoqaHanSansNeo = localFont({
+  src: [
+    {
+      path: "../../public/fonts/SpoqaHanSansNeo-Thin.woff2",
+      weight: "100",
+    },
+    {
+      path: "../../public/fonts/SpoqaHanSansNeo-Light.woff2",
+      weight: "300",
+    },
+    {
+      path: "../../public/fonts/SpoqaHanSansNeo-Regular.woff2",
+      weight: "400",
+    },
+    {
+      path: "../../public/fonts/SpoqaHanSansNeo-Medium.woff2",
+      weight: "500",
+    },
+    {
+      path: "../../public/fonts/SpoqaHanSansNeo-Bold.woff2",
+      weight: "700",
+    },
+  ],
+  display: "swap",
+  variable: "--font-spoqa-han-sans-neo",
+});
