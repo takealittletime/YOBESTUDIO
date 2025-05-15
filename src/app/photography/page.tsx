c5f345fae8e92b9c4a4f57ce4bbf9de1c87395f6
mkyu116 <mkyu116@krafton.com>
2025-05-15 17:35:43 +0900

    ✨ Feat: 기능 업데이트 1.0.0

--- src/app/photography/page.tsx
+++ src/app/photography/page.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/ui/footer/footer";
 import Header from "@/components/ui/header/header";
 
 import PhotographyContents from "./_components/PhotographyContents/page";
@@ -6,12 +7,13 @@ export default function Photography() {
   return (
     <div className="custom-scrollbar flex h-full flex-col text-{HelveticaWorld.variable}">
       <Header />
-      <div className="flex h-full flex-col bg-black">
-        <div className="my-3 flex justify-between p-3">
+      <div className="flex size-full flex-col bg-black">
+        <div className="my-3 flex flex-col justify-between p-3 sm:flex-row">
           <p className="text-5xl font-bold">Photography</p>
           <p>where style meets your vision</p>
         </div>
         <PhotographyContents />
+        <Footer />
       </div>
     </div>
   );
